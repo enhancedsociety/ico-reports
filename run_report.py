802401503b8b5e228ae764c92d35db8b185e7600
Filipe Gonçalves <filipe@impossible.com>
2018-07-05 16:55:59 +0100

    Add repository information to the report index Go deeper in the analysis except on the contract that causes mythril to go crazy.

--- run_report.py
+++ run_report.py
@@ -2,10 +2,22 @@
 
 import argparse
 import collections
+import datetime
 import os
 import shutil
 import subprocess
+from functools import reduce
+
 import yaml
+from git import Repo
+
+# * CAUTION *
+#
+# This script allows multiple configs to be provided, and as such
+# many repositories or many branches of the same repository could
+# in theory be analysed simultaneously. In practice, though, the
+# contract cache and the header information on git assume one and
+# only one repository being checked.
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -25,15 +37,35 @@ if __name__ == '__main__':
     # do not repeat analysis since it's a heavy op
     CONTRACT_CACHE = collections.defaultdict(bool)
 
+    header_info = {
+        'date': datetime.date.today().isoformat(),
+        'git_commit': 'N/A',
+        'git_ref': 'N/A',
+        'git_origin_url': 'N/A',
+    }
+
+    dir_under_git = os.path.abspath(os.path.join(
+        os.path.dirname(args.configs[0]), '..'))
+
+    repo = Repo(dir_under_git)
+    if not repo.bare and not repo.is_dirty():
+        configs_untracked = reduce(
+            lambda p, c: p or c in args.configs, repo.untracked_files, False)
+        contracts_untracked = len(list(filter(lambda c: c.endswith(
+            '.sol'), repo.untracked_files))) > 0
+        if not (configs_untracked or configs_untracked):
+            header_info['git_commit'] = repo.head.commit
+            header_info['git_ref'] = repo.head.reference
+            header_info['git_origin_url'] = repo.remotes.origin.url
+
     for c in args.configs:
         with open(c, 'r') as f:
-
             cy = yaml.safe_load(f)
 
             contracts = list()
 
             for eth_net in cy:
-                if type(cy[eth_net]) is dict and 'contracts' in cy[eth_net]: 
+                if type(cy[eth_net]) is dict and 'contracts' in cy[eth_net]:
                     cl = cy[eth_net]['contracts']
                     contracts.extend([cl[x]['contract_file'] for x in cl])
 
@@ -46,9 +78,11 @@ if __name__ == '__main__':
                 # in different folders, but for now we will ignore that edge
                 # case, for simplicity. Also, because so far it hasn't happened.
                 # Still, it's better to leave this comment if it ever does.
-                if not CONTRACT_CACHE[contract_file]:
+                if contract_file not in CONTRACT_CACHE:
                     CONTRACT_CACHE[contract_file] = True
                     contract_filename = os.path.basename(contract_file)
+                    # Gnosis + mythril = memory hike to infinity
+                    depth = 'shallow' if contract_filename == 'GnosisWallet.sol' else 'deepest'
                     shutil.copyfile(os.path.join(basepath, contract_file), os.path.join(
                         output_dir, contract_filename))
                     output_file = os.path.join(output_dir, '{}.{}'.format(
@@ -56,7 +90,8 @@ if __name__ == '__main__':
                     print('Writing {} report to {}'.format(
                         contract_file, output_file))
                     cmd = subprocess.run(
-                        [SOLSA_CMD, "-f", contract_file, "-o", output_file],
+                        [SOLSA_CMD, "-d", depth, "-f",
+                            contract_file, "-o", output_file, "-i"],
                         cwd=basepath, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                     if cmd.returncode != 0:
                         print(cmd.stdout)
@@ -116,11 +151,21 @@ if __name__ == '__main__':
             padding: 2px 16px;
         }
     </style>
-  </head>
+  </head>'''+'''
   <body>
-    <h1>Solsa Reports</h1>
+    <header>
+      <h1>Solsa Reports</h1>
+      <h5>{date}</h5>
+      <hr>
+      <aside>
+        <h4>git info</h4>
+        <p>Repository URL {git_origin_url}</p>
+        <p>Branch {git_ref}</p>
+        <p>Commit {git_commit}</p>
+      </aside>
+    </header>
     <main>
-'''
+'''.format(**header_info)
 
     TAIL = '''
     </main>
